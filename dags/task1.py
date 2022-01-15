65fced52af15bab86ce6cb68f338fe82335e3310
TomekM <tomasz_matuszewski@o2.pl>
2022-01-15 10:53:18 +0100

    development in progress

--- dags/task1.py
+++ dags/task1.py
@@ -1,5 +1,7 @@
 import json
 import logging
+from sqlite3 import Connection, Cursor
+from typing import Tuple
 import os
 import pandas as pd
 from requests import adapters, Session
@@ -11,11 +13,10 @@ from datetime import timedelta
 from dotenv import load_dotenv
 from pathlib import Path
 import sqlite3
+import time
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv(BASE_DIR / ".env")
-URL = os.getenv("URL")
-TOKEN = os.getenv("TOKEN")
 logger = logging.getLogger(__name__)
 
 args = {
@@ -23,10 +24,16 @@ args = {
     'start_date': days_ago(1)
 }
 
+# env vars
+DATABASE = os.getenv('DATABASE')
+CSV_PATH = os.getenv('CSV_PATH')
+URL = os.getenv("URL")
+TOKEN = os.getenv("TOKEN")
+
 dag = DAG(
     dag_id='spotify_job',
     default_args=args,
-    description="spotify data downloader",
+    description='spotify data downloader',
     schedule_interval=timedelta(days=1)
 )
 
@@ -46,7 +53,7 @@ def retryable_session(session: Session, retries: int = 8) -> Session:
     return session
 
 
-def get_data():
+def get_data() -> dict:
     session = retryable_session(Session())
     header = {"Authorization": f"Bearer {TOKEN}"}
     params = {"limit": 50, "after": 1484811043508}
@@ -60,33 +67,59 @@ def parse_json(data: dict) -> pd.DataFrame:
     df['url'] = df['artists_name'].apply(lambda col: col[0]['external_urls']['spotify'])
     df['artist_name'] = df['artists_name'].apply(lambda col: col[0]['name'])
     df['artist_id'] = df['artists_name'].apply(lambda col: col[0]['id'])
+    df['time'] = int(time.time())
     df.drop(columns=["artists_name"], inplace=True)
     return df
 
 
-def spotify_data_loader() -> None:
-    data = get_data()['items']
-    tracks_df = parse_json(data)
-    with sqlite3.connect(BASE_DIR / "spotify.db") as conn:
+def get_sqllite_session(db_name: str) -> Tuple[Connection, Cursor]:
+    with sqlite3.connect(BASE_DIR / db_name) as conn:
         cur = conn.cursor()
-        tracks_df.to_sql("tracks", conn, if_exists='append', index=False)
-        cur.execute("SELECT * from tracks")
-        logger.info(cur.fetchall())
+        return conn, cur
 
 
-def create_flat_csv_job():
-    logger.info("2ND TASK FINISHED")
+def csv_loader(db_name: str, table: str, destination_path: str) -> None:
+    conn, cur = get_sqllite_session(db_name)
+    df = pd.read_sql_query(f"SELECT * from {table}", conn)
+    df.to_csv(path_or_buf=BASE_DIR / destination_path)
 
+
+def spotify_data_loader(db_name, table) -> None:
+    try:
+        data = get_data()['items']
+        tracks_df = parse_json(data)
+        conn, cur = get_sqllite_session(db_name)
+        tracks_df.to_sql(table, conn, if_exists='append', index=False)
+        logger.info("Data loaded to sqlite database")
+    except Exception as e:
+        logger.error(f"Exception occured: {e}")
+
+
+def create_flat_csv_job(db_name: str, table: str, destination_path: str) -> None:
+    try:
+        csv_loader(db_name, table, destination_path)
+        logger.info("Data loaded to csv file")
+    except Exception as e:
+        logger.error(f"Exception occured: {e}")
 
 with dag:
-    run_this_task = PythonOperator(
+    load_data_sqllite = PythonOperator(
         task_id='load_data_to_sqllite',
-        python_callable=spotify_data_loader
+        python_callable=spotify_data_loader,
+        retries=3,
+        op_kwargs={'db_name': DATABASE, 'table': 'tracks'}
+
     )
 
-    run_this_task_too = PythonOperator(
-        task_id='print_final_statement',
-        python_callable=create_flat_csv_job
+    load_data_to_csv = PythonOperator(
+        task_id='load_data_to_csv',
+        python_callable=create_flat_csv_job,
+        op_kwargs={'db_name': DATABASE, 'table': 'tracks', 'destination_path': CSV_PATH}
     )
 
-    run_this_task >> run_this_task_too
+    load_data_sqllite >> load_data_to_csv
+
+
+# if __name__ == "__main__":
+#     spotify_data_loader("spotify.db", "tracks")
+#     create_flat_csv_job("spotify.db", "tracks", 'csv_results/tracks.csv')
